f23dcd6e6852d0f2fa50045efa33486abcba522d
Tomek Vivo <tomekdjersey1@gmail.com>
2025-05-10 15:25:19 +0100

    fix

--- src/pages/OnboardingPage.jsx
+++ src/pages/OnboardingPage.jsx
@@ -1,12 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import OnboardingFlow from "../components/Onboarding/OnboardingFlow";
-import OnboardingComplete from "../components/Onboarding/OnboardingComplete";
 
 function OnboardingPage() {
   return (
     <Routes>
       <Route path=":stepNumber" element={<OnboardingFlow />} />
-      <Route path="complete" element={<OnboardingComplete />} />
       <Route path="*" element={<p>No matching onboarding step.</p>} />
     </Routes>
   );
